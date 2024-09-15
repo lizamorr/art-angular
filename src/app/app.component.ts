cf0d092c07e9b45c08e7897e2a0501e98804943a
Liza Morrison <lizammorrison@gmail.com>
2024-09-15 19:03:10 +0200

    remove primeng and import hammermodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HammerModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './core/components/footer/footer.component';
@@ -7,7 +8,7 @@ import { HeaderComponent } from './core/components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, HammerModule],
   template: `<div class="flex flex-col h-screen">
     <core-header class="flex-shrink-0" /><router-outlet
       class="flex-grow"
